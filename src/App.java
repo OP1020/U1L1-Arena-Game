686c432cc584237a7d9dd2b9167552d177a0df13
OP1020 <peoplowe000@warren.k12.in.us>
2026-08-07 12:51:04 -0400

    COMMIT 1

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     final String myName = "owen";
+      System.out.println(myName);
+
+        char myLetter = '\u00A9';
+        System.out.println(myLetter);
+        
+
+        int x = 3;
+        byte b = 3;
+        short y = 3;
+        long z = 3L;
+        
+        double d = 3.14159265389;
+        float f = 3.14159265389F;
+
+        boolean myBool  = true;
+        boolean myBool2 = false;
+
+    }
+}   
+

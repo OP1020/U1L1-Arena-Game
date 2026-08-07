30d8c08459a62ff10e40770ae72facd866a9f1d1
OP1020 <peoplowe000@warren.k12.in.us>
2026-08-07 13:52:27 -0400

    commit 2

--- src/App.java
+++ src/App.java
@@ -1,23 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-     final String myName = "owen";
-      System.out.println(myName);
 
-        char myLetter = '\u00A9';
-        System.out.println(myLetter);
-        
+static final int MAX_HEALTH = 100;
+static final int STARTING_GOLD = 20;
 
-        int x = 3;
-        byte b = 3;
-        short y = 3;
-        long z = 3L;
-        
-        double d = 3.14159265389;
-        float f = 3.14159265389F;
+    public static void main(String[] args) throws Exception{
+        String playerName = "zawgalicious";
+        int health = MAX_HEALTH;
+        int gold = STARTING_GOLD;
+        boolean isAlive = true;
+        double critChance = 0.15;
 
-        boolean myBool  = true;
-        boolean myBool2 = false;
+        String enemyName = "lil baby man";
+        int enemyHealth = 15;
+        int enemyPower = 10;
 
     }
-}   
-
+}
